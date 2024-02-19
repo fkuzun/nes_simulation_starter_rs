7591c3de323934e8fc50c78830403ddf6d5dfb31
fkuzun <felix.s.lang@campus.tu-berlin.de>
2024-02-19 14:56:37 +0100

    do not create notebook

--- src/main.rs
+++ src/main.rs
@@ -201,7 +201,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let mut tuple_count_file = File::create(PathBuf::from(tuple_count_path)).unwrap();
                 tuple_count_file.write_all(tuple_count_string.as_bytes()).expect("Error while writing tuple count to file");
                 //create_notebook(&experiment.experiment_output_path, &PathBuf::from("/home/x/uni/ba/experiments/nes_experiment_input/Analyze-new.ipynb"), &experiment.generated_folder.join("analysis.ipynb"))?;
-                create_notebook(&PathBuf::from(&file_path), &PathBuf::from("/home/x/uni/ba/experiments/nes_experiment_input/Analyze-new.ipynb"), &experiment.generated_folder.join(format!("analysis_run{}.ipynb", attempt)))?;
+                //create_notebook(&PathBuf::from(&file_path), &PathBuf::from("/home/x/uni/ba/experiments/nes_experiment_input/Analyze-new.ipynb"), &experiment.generated_folder.join(format!("analysis_run{}.ipynb", attempt)))?;
                 if (shutdown_triggered.load(Ordering::SeqCst)) {
                     break;
                 }
